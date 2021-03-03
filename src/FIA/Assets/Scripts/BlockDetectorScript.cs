b92e4a1fbe7adaf6b37012639878e6060707117d
MikeLrUC <miguelrabuge12@gmail.com>
2021-03-03 10:51:36 +0000

    Added: Projct Custom Package

---
+++ src/FIA/Assets/Scripts/BlockDetectorScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDetectorScript : MonoBehaviour
+{
+
+    public float angleOfSensors = 10f;
+    public float rangeOfSensors = 10f;
+    protected Vector3 initialTransformUp;
+    protected Vector3 initialTransformFwd;
+    public float strength;
+    public float angleToClosestObj;
+    public int numObjects;
+    public bool debugMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        initialTransformUp = this.transform.up;
+        initialTransformFwd = this.transform.forward;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // YOUR CODE HERE
+
+    }
+
+    public float GetAngleToClosestObstacle()
+    {
+        return angleToClosestObj;
+    }
+
+    public float GetLinearOuput()
+    {
+        return strength;
+    }
+
+    public virtual float GetGaussianOutput()
+    {
+        // YOUR CODE HERE
+        throw new NotImplementedException();
+    }
+
+    public virtual float GetLogaritmicOutput()
+    {
+        // YOUR CODE HERE
+        throw new NotImplementedException();
+    }
+}
---
+++ src/FIA/Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class LinearRobotUnitBehaviour : RobotUnit
+{
+    public float weightResource;
+    public float resourceValue;
+    public float resouceAngle;
+
+    void Update()
+    {
+
+        // get sensor data
+        resouceAngle = resourcesDetector.GetAngleToClosestResource();
+
+        resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+
+        // apply to the ball
+        applyForce(resouceAngle, resourceValue); // go towards
+
+        
+
+    }
+
+
+}
+
+
+
+
+
+
