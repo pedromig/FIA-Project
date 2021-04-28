170142c6cd5a68feaa6bcf69974e6440452ec817
Pedro Miguel Duque Rodrigues <pedror@student.dei.uc.pt>
2021-04-28 16:46:56 +0100

    I think i finally got it

---
+++ src/TP-II/FIA/Assets/Scripts/LearningAlgorithms/TournamentSelection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class TournamentSelection : SelectionMethod
+{
+	private int tournamentSize;
+
+	public TournamentSelection(int tournamentSize) : base()
+	{
+		this.tournamentSize = tournamentSize;
+	}
+
+	public override List<Individual> selectIndividuals(List<Individual> oldpop, int num)
+	{
+		if (oldpop.Count < tournamentSize)
+		{
+			throw new System.Exception("The population size is smaller than the tournament size.");
+		}
+
+		List<Individual> selectedInds = new List<Individual>();
+		for (int i = 0; i < num; i++)
+		{
+			selectedInds.Add(tournamentSelection(oldpop,tournamentSize).Clone());
+		}
+
+		return selectedInds;
+	}
+
+	public Individual tournamentSelection(List<Individual> population, int tournamentSize)
+	{
+		//* YOUR CODE HERE *//
+		throw new System.NotImplementedException();
+	}
+}
